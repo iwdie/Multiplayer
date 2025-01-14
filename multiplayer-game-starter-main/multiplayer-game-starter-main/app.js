bb09f7d0df26bf801c17914c91b006c0d093060d
iwdie <42923352+iwdie@users.noreply.github.com>
2025-01-14 22:53:36 +0530

    added movements

--- multiplayer-game-starter-main/multiplayer-game-starter-main/app.js
+++ multiplayer-game-starter-main/multiplayer-game-starter-main/app.js
@@ -4,10 +4,10 @@ const app = express()
 const http =  require('http')
 const server = http.createServer(app)
 const { Server } = require('socket.io')
-const io = new Server(server)
+
+const io = new Server(server, {pingInterval: 2000, pingTimeout: 5000})
 
 const port = 3000
-
 
 app.use(express.static('public'))
 
@@ -15,20 +15,40 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-const players = {}
-
-
+const players = {};
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
-  players[socket.id] = {
-    x: 300,
-    y: 300
-  }
-  io.emit('updatePlayers', backendPlayers)
+  console.log('a user connected'); 
+  players[socket.id] = { x: 500*Math.random(), y: 500*Math.random() };
 
-  console.log(players)
-})
+  io.emit('updatePlayers', players);
+
+  socket.on('disconnect', (reason) => {
+    delete players[socket.id];
+  });
+
+  socket.on('keydown', (keycode) => {
+    switch(keycode){
+      case 'KeyW':
+        players[socket.id].y -= 10;
+        break;
+      case 'KeyA':
+        players[socket.id].x -= 10;
+        break;
+      case 'KeyS':
+        players[socket.id].y += 10;
+        break;
+      case 'KeyD':
+        players[socket.id].x += 10;
+    }
+  });
+
+  console.log(players);
+});
+
+setInterval(() => {
+  io.emit('updatePlayers', players);
+}, 15);
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- multiplayer-game-starter-main/multiplayer-game-starter-main/public/js/index.js
+++ multiplayer-game-starter-main/multiplayer-game-starter-main/public/js/index.js
@@ -12,15 +12,28 @@ const y = canvas.height / 2
 
 const player = new Player(x, y, 10, 'white')
 const players = {}
-socket.on('updatePlayers', (backendPlayers) => {
-  for(const id in backendPlayers){
-    const backendPlayers = backendPlayers[id]
-    if(!players[id]){
-      players[id] = new Player(backendPlayers.x, backendPlayers.y, 10, 'white')
-    }
+
+socket.on('updatePlayers', (BackendPlayers) => {
+ for (const id in BackendPlayers) {
+    const player = BackendPlayers[id]
+    players[id] = new Player(player.x, player.y, 10, 'white')
+  
+  if(!players[id]){
+    players[id] = new Player(x, y, 10, 'white')
   }
-  console.log(players)
-})
+  else{
+    players[id].x = player.x
+    players[id].y = player.y
+  }
+}
+   for (const id in players){
+    if(!BackendPlayers[id]){
+      delete players[id]
+    }
+   }
+
+
+});
 
 
 let animationId
@@ -29,14 +42,38 @@ function animate() {
   animationId = requestAnimationFrame(animate)
   c.fillStyle = 'rgba(193, 10, 110, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
-
   for (const id in players) {
-    const player = players[id]
-    player.draw()
+      const player = players[id]
+      player.draw()
   }
+
+ 
 
    
 }
 
 animate()
+
+window.addEventListener('keydown',(event)=>{
+  if(!players[socket.id]) return
+  
+  switch(event.code){
+    case 'KeyW':
+      players[socket.id].y -= 10
+      socket.emit('keydown','KeyW')
+      break
+    case 'KeyA':
+      players[socket.id].x -= 10
+      socket.emit('keydown','KeyA')
+      break
+    case 'KeyS':
+      players[socket.id].y += 10
+      socket.emit('keydown','KeyS')
+      break
+    case 'KeyD':
+      players[socket.id].x += 10
+      socket.emit('keydown','KeyD')
+      break
+  }
+})
 
